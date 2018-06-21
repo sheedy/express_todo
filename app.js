1e3cd9b1672573694a43b3a8c13fde9936e227f4
dustinpfister <dustin.pfister@gmail.com>
2018-06-21 18:58:57 -0400

    using ejs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let express = require('express'),
 app = express();
 
-//app.set('port', 8080);
 
-// settings
+// use lib/conf.js to load app settings from conf.yaml
 require('./lib/conf.js')(app, __dirname).then(function () {
+	
+	app.get('/', function(req,res){
+		
+		res.render('index');
+		
+	});
 
     app.listen(app.get('port'), function () {
 
