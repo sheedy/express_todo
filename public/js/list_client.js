c3b9c786d8b81b48f5334661016147783c11316f
dustinpfister <dustin.pfister@gmail.com>
2018-06-22 15:59:22 -0400

    list_client

--- public/js/list_client.js
+++ public/js/list_client.js
@@ -1,2 +1,41 @@
 
-console.log('list_client.js');
+// list client.
+var lc = (function () {
+
+    // simple, custom, http client using XMLHttpRequest
+    var http = function (obj) {
+
+        obj = obj || {};
+
+        // defaults to making GET requests to the /list path
+        this.method = obj.method || 'GET';
+        this.path = obj.path || '/list';
+        this.payload = obj.payload || null;
+        this.onDone = obj.OnDone || function () {
+            console.log(this.response);
+        };
+        this.onFail = obj.OnFail || function () {
+            console.log(this.response);
+        };
+
+        // start the request
+        var xhr = this.xhr = new XMLHttpRequest();
+        xhr.open(this.method, this.path);
+        // with this client all GET request should be for JSON
+        if (this.method.toUpperCase() === 'GET') {
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        }
+        xhr.onreadystatechange = function () {
+
+            console.log(this);
+
+        };
+
+        xhr.send(this.payload);
+
+    };
+
+    var req = new http();
+
+}
+    ());
