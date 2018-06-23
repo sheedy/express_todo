1f2cebccc14eb329f8259a627db72b34b74d2edc
dustinpfister <dustin.pfister@gmail.com>
2018-06-23 10:26:23 -0400

    edit path responds with lists if no id is given

--- routes/edit.js
+++ routes/edit.js
@@ -1,21 +1,47 @@
 
 let express = require('express'),
+dbLists = require('../lib/db_lists'),
 
 editApp = express();
 
-editApp.get('/edit', function(req, res) {
+editApp.get('/edit', function (req, res) {
 
-    res.render('index', {
-        layout: 'edit',
-        listId: req.query.l || null
-    });
+    if (req.query.l === undefined) {
+
+        dbLists.readList().then(function (list) {
+
+            res.render('index', {
+                layout: 'edit',
+                listId: null,
+                lists: list.get('lists').value()
+            });
+
+        }).catch (function () {
+
+            res.render('index', {
+                layout: 'edit',
+                listId: null,
+                lists: []
+            });
+
+        });
+
+    } else {
+
+        res.render('index', {
+            layout: 'edit',
+            listId: req.query.l || null,
+            lists: []
+        });
+
+    }
 
 });
 
 module.exports = function (obj) {
 
     // set views from main app
-    editApp.set('views',obj.app.get('views'));
+    editApp.set('views', obj.app.get('views'));
 
     return editApp;
 
--- themes/landscape/js/edit.js
+++ themes/landscape/js/edit.js
@@ -1,13 +1,18 @@
 
 // use list clients createList Method to send the body
-lc.getList({
 
-    listId: get('listid').innerText,
-    onDone: function () {
+if (get('listId')) {
 
-        console.log('got list');
-        console.log(this.response);
+    lc.getList({
 
-    }
+        listId: get('listid').innerText,
+        onDone: function () {
 
-});
+            console.log('got list');
+            console.log(this.response);
+
+        }
+
+    });
+
+}
